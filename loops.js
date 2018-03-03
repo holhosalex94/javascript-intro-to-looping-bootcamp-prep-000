4d7d499d5a3ac7e530e5bfa9bca90dfa5475e401
Alex Holhos <alex.holhos@emanuel.ro>
2018-03-03 11:07:17 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array){
   for(i=0; i<25;i++){
-    return "I am "+ i + "strange loop"+ (i ===0 ? '' : 's');
+   array.push("I am "+ i + "strange loop"+ (i ===0 ? '' : 's'));
   }
   
 }
